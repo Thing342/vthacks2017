f1bb88d1c81d01dd63147aefd0151a7931424873
Thing342 <wesjordansemailaddress@gmail.com>
2017-02-19 01:47:52 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vthacks17',
+    version='0.1',
+    packages=['server'],
+    url='',
+    license='',
+    author='wes',
+    author_email='wes@wesj.org',
+    description='VTHacks 2017 submission',
+    install_requires=['flask', 'googlemaps', 'requests'],
+)
